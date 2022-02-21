afe9bd2c9ff67b4eeef40d8cb9ff03d06cdda2ac
BhargavMN <bhargav.malasaninagaraj@student.put.poznan.pl>
2022-02-21 11:18:32 +0100

    UART and LCD Refactor
    Co-Authored-By: WojciechPiersiala <99994418+WojciechPiersiala@users.noreply.github.com>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,7 +58,7 @@ float U, Y, E;
 
 
 
-char buff[64];
+char buff[16];
 int sampling_freq = 10;
 float total_time;
 
@@ -84,7 +84,7 @@ float D;
 
 int new_duty;
 
-char msg_[64];
+char msg_[32];
 int status_len;
 
 uint32_t encoder_coutner = 0;
@@ -119,41 +119,41 @@ int _write(int file, char *ptr, int len)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
-  MX_TIM3_Init();
-  MX_TIM4_Init();
-  MX_TIM2_Init();
-  MX_TIM5_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART3_UART_Init();
+	MX_USB_OTG_FS_PCD_Init();
+	MX_TIM3_Init();
+	MX_TIM4_Init();
+	MX_TIM2_Init();
+	MX_TIM5_Init();
+	MX_I2C1_Init();
+	/* USER CODE BEGIN 2 */
 
 	//usart
 	HAL_UART_Receive_IT(&huart3, &buff,3);
@@ -180,21 +180,24 @@ int main(void)
 
 	//LCD
 
-	  disp.addr = (0x3F << 1);
-	  disp.bl = true;
-	  lcd_init(&disp);
-//	  sprintf((char *)disp.f_line, "To 1. linia");
-//	  sprintf((char *)disp.s_line, "a to druga linia");
-//	  lcd_display(&disp);
+	disp.addr = (0x3F << 1);
+	disp.bl = true;
+	lcd_init(&disp);
+	//	  sprintf((char *)disp.f_line, "To 1. linia");
+	//	  sprintf((char *)disp.s_line, "a to druga linia");
+	//	  lcd_display(&disp);
 
-	  char LCDdisplay1[17];
-	  char LCDdisplay2[17];
-  /* USER CODE END 2 */
+	char LCDdisplay1[17];
+	char LCDdisplay2[17];
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
+
 		sprintf(LCDdisplay1, "U: %d rpm",(int)(U*60));
 		sprintf(LCDdisplay2, "Y: %d rpm",(int)(Y*60));
 
@@ -203,74 +206,76 @@ int main(void)
 
 		lcd_display(&disp);
 
-		HAL_Delay(500);
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+
+		HAL_Delay(500);
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
-  /** Configure LSE Drive Capability
-  */
-  HAL_PWR_EnableBkUpAccess();
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 216;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Configure LSE Drive Capability
+	 */
+	HAL_PWR_EnableBkUpAccess();
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 216;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Activate the Over-Drive mode
+	 */
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_I2C1
-                              |RCC_PERIPHCLK_CLK48;
-  PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
-  PeriphClkInitStruct.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
-  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_I2C1
+			|RCC_PERIPHCLK_CLK48;
+	PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+	PeriphClkInitStruct.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+	PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -321,7 +326,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
  @return None/* @version V1.0
  @date    17-Feb-2022
 
-*****************************************************************************************************/
+ *****************************************************************************************************/
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 	encoder_coutner=__HAL_TIM_GET_COUNTER(htim);
 	count=((int16_t)encoder_coutner)/4;
@@ -338,35 +343,9 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 
 	}
 
-////	U_tmp+=count;
-//		U_tmp=10;
+	////	U_tmp+=count;
+	//		U_tmp=10;
 }
-//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-//	if(huart->Instance == USART3){
-//		HAL_UART_Receive_IT(&huart3, buff, 3);
-//		char inx0 =buff[0];
-//
-//		switch(inx0){
-//		case ('S'):{
-//			int val0, val1, new_duty;
-//			val0 = (int)(buff[1]-'0');
-//			val1 = (int)(buff[2]-'0');
-//			new_duty=(val0*10+val1)*100;
-//			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,new_duty);
-//
-//			break;
-//		}
-//		case ('U'):{
-//					int val0, val1, new_duty;
-//					val0 = (int)(buff[1]-'0');
-//					val1 = (int)(buff[2]-'0');
-//					U_tmp=val0*10+val1;
-//					count=0;   ////////////////////// count test
-//					break;
-//				}
-//		}
-//	}
-//}
 /*****************************************************************************************************
  @brief   UART Callback(Serail communication interface to read and write the values)
  @author  Bhargav Malasani,Wojciech Piersiala
@@ -376,51 +355,51 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
  @date    20-Feb-2022
 
 /*****************************************************************************************************/
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if(huart->Instance == USART3){
-			HAL_UART_Receive_IT(&huart3, buff, 3);
+		HAL_UART_Receive_IT(&huart3, buff, 3);
+		char inx0 =buff[0];
 
-			if(strncmp(buff,'SETD',4)==0)
-			{
+		switch(inx0){
+		case ('S'):{
+			int val0, val1, new_duty;
+			val0 = (int)(buff[1]-'0');
+			val1 = (int)(buff[2]-'0');
+			new_duty=(val0*10+val1)*100;
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,new_duty);
 
-				int val0, val1, new_duty;
-				val0 = (int)(buff[4]-'0');
-				val1 = (int)(buff[5]-'0');
-				new_duty=(val0*10+val1)*100;
-				__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,new_duty);
-				status_len=sprintf(msg_,"Duty has been Set");
-			}
-			else
-				if(strncmp(buff,'SETU',4)==0)
-				{
-					int val0, val1, new_duty;
-					val0 = (int)(buff[4]-'0');
-					val1 = (int)(buff[5]-'0');
-					U_tmp=val0*10+val1;
-					count=0;   ////////////////////// count test
-					status_len=sprintf(msg_,"Refernce value has been Set");
-				}
-				else
-					if(strncmp(buff,'READ:U',6)==0)
-				{
+			break;
+		}
+		case ('U'):{
+			int val0, val1, new_duty;
+			val0 = (int)(buff[1]-'0');
+			val1 = (int)(buff[2]-'0');
+			U_tmp=val0*10+val1;
+			count=0;   ////////////////////// count test
+			break;
+		}
+		case ('R'):{
+			char USARTdisp3[17];
+			int len2;
+			if(buff[2]=='Y'){
 
-						status_len=sprintf(msg_,"Refernce Value: %d RPM\r\n",(U*60));
-				}
-				else
-					if(strncmp(buff,'READ:Y',6)==0)
-					{
+			len2=sprintf(USARTdisp3, "Y: %d rpm \n\r",(int)(Y*60));}
 
-						status_len=sprintf(msg_,"Speed: %d RPM\r\n",(Y*60));
-					}
-					else
-					if(strncmp(buff,'READ:E',6)==0)
-					{
-						status_len=sprintf(msg_,"Error: %d \r\n",E);
-					}
-			HAL_UART_Transmit( &huart3,msg_,status_len, 100);
+			if(buff[2]=='U'){
+
+						len2=sprintf(USARTdisp3, "U: %d rpm \n\r",(int)(U*60));}
+
+			if(buff[2]=='E'){
+
+						len2=sprintf(USARTdisp3, "E: %d rpm \n\r",(int)(E)*60);}
+
+			HAL_UART_Transmit(&huart3, &USARTdisp3, len2, 100);
+			break;
+		}
+	}
 }
 }
+
 
 
 #if 1
@@ -440,19 +419,22 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 		total_time+=1/((float)sampling_freq);
 		Y=rot_freq;
 		U=U_tmp+count;
-
+		if(U<0){
+			U=0;
+			count=0;
+		}
 		E=U-Y;
 
-//I
+		//I
 		I = prev_I+ E+prev_E;
 		Duty_test_I=I*k_I;
 		if(Duty_test_I<1){Duty_test_I=1;}if(Duty_test_I>MAX_DUTY){Duty_test_I=MAX_DUTY; I=MAX_DUTY/k_I;}
 
-//P
+		//P
 		Duty_test_P=E*k_E;
 		if(Duty_test_P<1){Duty_test_P=1;}if(Duty_test_P>MAX_DUTY){Duty_test_P=MAX_DUTY;}
 
-//D
+		//D
 		D = (E-prev_E);
 		Duty_test_D=k_D*D;
 		if(Duty_test_D<-900){Duty_test_D=-900;}if(Duty_test_D>MAX_DUTY){Duty_test_D=MAX_DUTY;}
@@ -462,44 +444,44 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 		prev_E=E;
 		prev_I=I;
 
-	if(Duty_test<1){Duty_test=1;}if(Duty_test>MAX_DUTY){Duty_test=MAX_DUTY;}
+		if(Duty_test<1){Duty_test=1;}if(Duty_test>MAX_DUTY){Duty_test=MAX_DUTY;}
 
-	__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,Duty_test);
+		__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1,Duty_test);
 	}
-//	printf("%f %f\n\r",total_time,rot_freq);
+	//	printf("%f %f\n\r",total_time,rot_freq);
 }
 #endif
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
